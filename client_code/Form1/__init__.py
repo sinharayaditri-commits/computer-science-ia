5cc5a17e483c6045e7243f5bc84a0e7ddb5af862
sinharayaditri@gmail.com <sinharayaditri@gmail.com>
2025-09-30 04:16:04 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -5,10 +5,19 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     self.init_components(**properties)
 
-    # Prompt user to log in
-    user = anvil.users.login_with_form()
+    action = anvil.alert("Do you want to Log In or Sign Up?", buttons=["Log In", "Sign Up"])
+    if action == "Sign Up":
+      user = anvil.users.signup_with_form()
+      anvil.alert("Sign up successful! Please ask your admin to set your role and enable your account if required.")
+      user = anvil.users.login_with_form()
+    else:
+      user = anvil.users.login_with_form()
+
+    print('User object:', user)
+    print('Role property:', user['role'] if 'role' in user else None)
+
     if user:
-      role = user.get('role', None)
+      role = user['role'] if 'role' in user else None
       if role == 'admin':
         from ..AdminDashboard import AdminDashboard
         self.clear()
