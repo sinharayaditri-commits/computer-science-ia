c8236035ea33ae11a1fe3b359ece4f066c8d79f9
sinharayaditri@gmail.com <sinharayaditri@gmail.com>
2025-12-01 08:41:33 +0000

    Edited TeacherDashboard.RowTemplate3

--- client_code/TeacherDashboard/RowTemplate3/__init__.py
+++ client_code/TeacherDashboard/RowTemplate3/__init__.py
@@ -4,13 +4,3 @@ from anvil import *
 class RowTemplate3(RowTemplate3Template):
   def __init__(self, **properties):
     self.init_components(**properties)
-
-    # DEBUG - Is this even running?
-    print(f"ROW TEMPLATE: Loading row with data: {self.item}")
-
-    self.title_label. text = self.item.get('title', '')
-    self.urgency_label.text = self.item. get('urgency', '')
-    self. status_label.text = self.item. get('status', '')
-    self.location_label.text = self.item.get('location', '')
-
-    print(f"ROW TEMPLATE: Set labels successfully!")
