95ed6cf2cb9f56ef220d80dadfabf4537170d029
sinharayaditri@gmail.com <sinharayaditri@gmail.com>
2025-12-09 03:08:06 +0000

    Edited TeacherDashboard.RowTemplate3

--- client_code/TeacherDashboard/RowTemplate3/__init__.py
+++ client_code/TeacherDashboard/RowTemplate3/__init__.py
@@ -4,3 +4,9 @@ from anvil import *
 class RowTemplate3(RowTemplate3Template):
   def __init__(self, **properties):
     self.init_components(**properties)
+
+    # Display the issue data in each label
+    self. title_label.text = self.item.get('title', '')
+    self.urgency_label.text = self.item.get('urgency', '')
+    self.status_label.text = self.item.get('status', '')
+    self.location_label.text = self. item.get('location', '')
